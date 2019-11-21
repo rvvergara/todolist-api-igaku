9d587103dbb8917b7115a24c82b1a988c7e26a6b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 13:46:22 +0800

    Add owner field to Todo schema

--- src/models/todo.js
+++ src/models/todo.js
@@ -9,6 +9,11 @@ const TodoSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
