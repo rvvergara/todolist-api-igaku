6e87724968b30142f37c671aeef6bb80d77a1b42
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 11:33:05 +0800

    Add user authentication
    - Add instance method generateAuthToken in order to create and send user
    token every successful login
    - Add static method findByCredentials in order to find a user based on
    email and password
    - Create user creation and user authentication routes in index.js
    (temporary location)

--- src/index.js
+++ src/index.js
@@ -12,12 +12,23 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/v1/users', async (req, res) => {
-  const user = new User(req.body);
+  const user = await new User(req.body);
+
   try {
     await user.save();
     res.status(201).send(user);
   } catch (e) {
     res.status(422).send(e);
+  }
+});
+
+app.post('/v1/sessions', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(401).send(e);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -28,8 +29,17 @@ const UserSchema = new mongoose.Schema({
     trim: true,
     minLength: 7,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
+// Middlewares
 UserSchema.pre('save', async function (next) {
   const user = this;
   if (user.isModified('password')) {
@@ -38,4 +48,39 @@ UserSchema.pre('save', async function (next) {
   return next();
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Static methods
+// 1. Helper method for logging in a user using email and password
+UserSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Email error');
+  }
+  const isAuthenticated = await bcrypt.compare(password, user.password);
+  if (!isAuthenticated) {
+    throw new Error('Password error');
+  }
+  return user;
+};
+
+// Instance methods
+
+// 1. Generating a user token based on user._id -> concatenate to user's tokens collection
+// - this is in order to log out a user properly per device
+UserSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    {
+      _id: user._id.toString(),
+    },
+    'mysecretkey',
+    {
+      expiresIn: '1 day',
+    },
+  );
+  user.tokens = [...user.tokens, { token }];
+  await user.save();
+  return token;
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
