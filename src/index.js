c19de6038bc18db657118b8954e961c9764dce4e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 15:10:23 +0800

    Install and use cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const userRouter = require('./routes/user');
 const sessionRouter = require('./routes/session');
 const todoRouter = require('./routes/todo');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
