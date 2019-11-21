86b458632b52920eb4400807b2bad58165cbe4ff
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 14:02:42 +0800

    Create routes and handlers for logging out a user

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -9,3 +9,26 @@ exports.create = async (req, res) => {
     res.status(401).send(e);
   }
 };
+
+exports.destroy = async (req, res) => {
+  try {
+    const user = await req.user;
+    user.tokens = user.tokens.filter(({ token }) => token !== req.token);
+    await user.save();
+    res.status(202).send({ success: 'Successfully logged out' });
+  } catch (e) {
+    res.status(500).send({ error: 'Something went wrong' });
+  }
+};
+
+// Log out of all sessions
+exports.destroyAll = async (req, res) => {
+  try {
+    const user = await req.user;
+    user.tokens = [];
+    await user.save();
+    res.status(202).send({ success: 'Successfully logged out from all devices' });
+  } catch (e) {
+    res.status(500).send({ error: 'Something went wrong' });
+  }
+};
