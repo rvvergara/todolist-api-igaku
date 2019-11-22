c1aa9135261d35070829c0d5e1b5f55804d05646
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-22 17:38:05 +0800

    Hide mongodb url and port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const todoRouter = require('./routes/todo');
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
